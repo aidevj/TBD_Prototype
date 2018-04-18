5e124d41c081c0a2894e080ec7f34c7510f992c8
Erin McAnany <elm6208@rit.edu>
2018-04-18 14:35:08 -0400

    Added basic enemy script template
    Started writing enemy script, created empty template for Ability

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour {
+
+    //placeholder for now, will fill out later
+    public int cost;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
