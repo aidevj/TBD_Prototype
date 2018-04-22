7efbfd19aa1b85ef4d3c3a615aa079982a3b831c
Aiden Melendez <itm5305@rit.edu>
2018-04-22 01:07:37 -0400

    grid stuff works
    - all work in GridScene
    - procedurally generated at runtime ya gotta run it just trust me tutorials are life
    - test function: click to change color or hex
    
    **If unity decides to forget about its UI Namespace: Assets > Reimport All

---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A struct to represent coordinates in a Hex system
+/// </summary>
+[System.Serializable]
+public struct HexCoordinates {
+
+	public int X { get; private set; }
+
+	public int Z { get; private set; }
+
+	public int Y {
+		get {
+			return -X - Z;
+		}
+	}
+
+	public HexCoordinates (int x, int z) {
+		X = x;
+		Z = z;
+	}
+
+	// static method overloaded constructor to create a set of coordinates using regular offset coordinates
+	public static HexCoordinates FromOffsetCoordinates (int x, int z) {
+		return new HexCoordinates(x - z / 2, z);
+	}
+
+	//static method overloaded constructor to construct a hex coordinate from the position given
+	public static HexCoordinates FromPosition (Vector3 position) {
+		float x = position.x / (HexMetrics.innerRadius * 2f);
+		float y = -x;
+
+		// every two rows, shift one unit to the left
+		float offset = position.z / (HexMetrics.outerRadius * 3f);
+		x -= offset;
+		y -= offset;
+
+		// round the itnergers to get the coordinates
+		int iX = Mathf.RoundToInt(x);
+		int iY = Mathf.RoundToInt(y);
+		int iZ = Mathf.RoundToInt(-x -y);
+
+		//  when coordinates don't add to zero
+		if (iX + iY + iZ != 0) {
+			//Debug.LogWarning("rounding error!");
+
+			// happens at edges of between hexes, so assume the coordinate that got rounded the most is incorrect
+			float dX = Mathf.Abs(x - iX);
+			float dY = Mathf.Abs(y - iY);
+			float dZ = Mathf.Abs(-x -y - iZ);
+
+			if (dX > dY && dX > dZ) {
+				iX = -iY - iZ;
+			}
+			else if (dZ > dY) {
+				iZ = -iX - iY;
+			}
+		}
+
+		return new HexCoordinates(iX, iZ);
+	}
+
+	// override toStrings so it doesn't just reutrn the struct's type name
+	public override string ToString () {
+		return "(" + X.ToString() + ", " + Y.ToString() + ", " + Z.ToString() + ")";
+	}
+
+	public string ToStringOnSeparateLines () {
+		return X.ToString() + "\n" + Y.ToString() + "\n" + Z.ToString();
+	}
+}
---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script for the HexGrid object. Handles creating and trangulating the meshes of the cells of the grid.
+/// Also sets coordinates and writes them on grid.
+/// </summary>
+public class HexGrid : MonoBehaviour {
+
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	Canvas gridCanvas;
+	HexMesh hexMesh;
+
+	HexCell[] cells;
+
+	public Color defaultColor = Color.white;
+	public Color activeColor = Color.cyan; // "touched"
+
+	void Awake () {
+		cells = new HexCell[height * width];
+
+		gridCanvas = GetComponentInChildren<Canvas> ();
+		hexMesh = GetComponentInChildren<HexMesh> ();
+
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void Start(){
+		// after the grid has awoken
+		hexMesh.Triangulate (cells);
+	}
+
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			HandleInput();
+		}
+	}
+
+	void HandleInput () {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit)) {
+			TouchCell(hit.point);
+		}
+	}
+
+	void TouchCell (Vector3 position) {
+		position = transform.InverseTransformPoint(position);
+
+		// to know where we are touching, we have to convert the touch position to hex coordinates
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		Debug.Log("touched at " + coordinates.ToString());
+
+		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+		HexCell cell = cells[index];
+		cell.color = activeColor;
+		hexMesh.Triangulate(cells);
+	}
+
+	/// <summary>
+	/// Creates cells at the given positions.
+	/// </summary>
+	/// <param name="x">The x position.</param>
+	/// <param name="z">The z position.</param>
+	/// <param name="i">Index of the cell out of all.</param>
+	void CreateCell (int x, int z, int i) {
+		Vector3 position;
+		// space them out according to radius and offset
+		position.x = ( x + z * 0.5f  - z / 2 ) * (HexMetrics.innerRadius * 2f); //position.x = x * 10f; // <-- non shifted
+		position.y = 0f;
+		position.z = z * (HexMetrics.outerRadius * 1.5f); //position.z = z * 10f;
+
+		// assign cell to the current index and assign its values
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
+
+		// instantiate the labels and show cell coordinates
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = cell.coordinates.ToStringOnSeparateLines();
+	}
+
+}
+
+// reference: http://catlikecoding.com/unity/tutorials/hex-map/part-1/
