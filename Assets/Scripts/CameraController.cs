5e97dc91ad39896a01e2d1c6e74e68d39ee5fb05
Aiden Melendez <itm5305@rit.edu>
2018-04-22 01:16:43 -0400

    camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿//Aiden
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical"), 0);
+	}
+}
